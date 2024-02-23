9102372bdbf5e72146ea81f83ad95f18edfc3571
021-projects <20326979+021-projects@users.noreply.github.com>
2024-02-23 15:05:45 +0200

    tests(tx): reset currency scaling on setup

--- tests/TransactionTest.php
+++ tests/TransactionTest.php
@@ -21,6 +21,14 @@ class TransactionTest extends TestCase
     use BalanceSeed;
     use WithFaker;
 
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // Reset the scaling for the currencies to prevent test failing with small values
+        config(['wallet.currency_scaling' => []]);
+    }
+
     public function test_deposit(): void
     {
         [$user, $currency, $balance] = $this->createBalance();
